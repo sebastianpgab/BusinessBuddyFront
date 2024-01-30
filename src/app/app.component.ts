9fa4b10a78dba7e5b2aee7532b8b9ec9996217e8
sebastianpgab <sebastianpgab@gmail.com>
2024-01-30 23:57:48 +0100

    Learn event and property binding in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+
    <h1>Przykłady interpolacji</h1>
    <p>Imie {{name.toUpperCase()}}</p>
    <p>Wiek {{25}}</p>
    <p>Jest dorosły {{isAdult}}</p>
-   <p>----------------</p>
-   <h1>Przykład propery bindingu</h1>
-   <p [class.ChangeColour] = "isActiv">Dupa</p>
 
+   <p>----------------</p>
+   <h1>Przykład property bindingu</h1>
+   <p [class.changeColour] = "isActiv">Dupa</p>
+   <button [disabled] = "isActiv">Kliknij</button>
+   <div [style.color]="isHighlighted ? 'red' : 'blue'">Kolor</div>
+   <div [class.changeFont]= "changeFont">Tekst Tekst Tekst</div>
+   <input [value]= "name">
+
+   <p>----------------</p>
+   <h1>Przykłady event bindingu</h1>
+   <button (click)="changeFontMethod()" type="button" >Zmień czcionkę</button>
   `,
-  styles: [ '.ChangeColour {color: blue}']
+  styles: [ 
+ '.changeColour {color: blue}',
+ '.spacialStyle {color: red; text-decoration: underline; }',
+ '.changeFont {font-family: "Arial"}'
+]
 })
+
+
 export class AppComponent {
   name = 'Sebastian';
   age = 25
   isAdult = false;
-  isActiv = true;
+  isActiv = false;
+  isHighlighted = true;
+  changeFont:boolean = true;
+
+  changeFontMethod() {
+  this.changeFont = !this.changeFont; 
+}
+
+
+
 }
